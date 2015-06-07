e592ceba7723a7ee19af9b59a6cb4ba903382b25
mahesh-wohlig <mahesh@wohlig.com>
2015-06-07 15:42:30 +0530

    done
    sssd

---
+++ www/js/controllers.js
@@ -0,0 +1,4 @@
+angular.module('starter.controllers', [])
+.controller('AppCtrl', function($scope) {})
+.controller('UserloginCtrl', function($scope, $stateParams) {})
+.controller('UsersignupCtrl', function($scope, $stateParams) {});
