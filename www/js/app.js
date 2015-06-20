3c9179db906181af1c747d826b9a5b2628b84457
Tushar <tushar@wohlig.com>
2015-06-20 13:36:43 +0530

    Few fixes

--- www/js/app.js
+++ www/js/app.js
@@ -4,54 +4,60 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
+angular.module('starter', ['ionic', 'starter.controllers', 'wu.masonry'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleLightContent();
+            StatusBar.overlaysWebView(true);
+        }
+        if (cordova.platformId == 'android') {
+            StatusBar.backgroundColorByHexString("#9C27B0");
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    //Ionic native scrolling
+    $ionicConfigProvider.scrolling.jsScrolling(false);
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-.state('user-login', {
-      url: "/user-login",
-          templateUrl: "templates/user-login.html",
-          controller: 'UserloginCtrl'
-    })
-  .state('user-signup', {
-      url: "/user-signup",
-     
-          templateUrl: "templates/user-signup.html",
-          controller: 'UsersignupCtrl'
-     
-    })
+    $stateProvider
 
-     .state('app.home', {
-            url: "/home",
-            views: {
-                'menuContent': {
-                    templateUrl: "templates/home.html",
-                    controller: 'HomeCtrl'
-                }
-            }
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
+    })
+        .state('user-login', {
+            url: "/user-login",
+            templateUrl: "templates/user-login.html",
+            controller: 'UserloginCtrl'
         })
-    .state('app.contact', {
+        .state('user-signup', {
+            url: "/user-signup",
+
+            templateUrl: "templates/user-signup.html",
+            controller: 'UsersignupCtrl'
+
+        })
+
+    .state('app.home', {
+        url: "/home",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/home.html",
+                controller: 'HomeCtrl'
+            }
+        }
+    })
+        .state('app.contact', {
             url: "/contact",
             views: {
                 'menuContent': {
@@ -60,7 +66,7 @@ angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
                 }
             }
         })
-    .state('app.setting', {
+        .state('app.setting', {
             url: "/setting",
             views: {
                 'menuContent': {
@@ -69,7 +75,7 @@ angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
                 }
             }
         })
-      .state('app.gallery', {
+        .state('app.gallery', {
             url: "/gallery",
             views: {
                 'menuContent': {
@@ -78,7 +84,7 @@ angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
                 }
             }
         })
-      .state('app.about', {
+        .state('app.about', {
             url: "/about",
             views: {
                 'menuContent': {
@@ -87,7 +93,7 @@ angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
                 }
             }
         })
-      .state('app.faq', {
+        .state('app.faq', {
             url: "/faq",
             views: {
                 'menuContent': {
@@ -96,44 +102,44 @@ angular.module('starter', ['ionic', 'starter.controllers','wu.masonry'])
                 }
             }
         })
-     .state('app.articles', {
-      url: "/articles",
-       views: {
+        .state('app.articles', {
+            url: "/articles",
+            views: {
                 'menuContent': {
-          templateUrl: "templates/articles.html",
-          controller: 'ArticlesCtrl'
+                    templateUrl: "templates/articles.html",
+                    controller: 'ArticlesCtrl'
                 }
-       }
-    })
-  ;
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/user-login');
+            }
+        });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/user-login');
 })
 
 .filter('inSlicesOf', ['$rootScope',
-         function ($rootScope) {
-            makeSlices = function (items, count) {
-                if (!count)
-                    count = 3;
+    function($rootScope) {
+        makeSlices = function(items, count) {
+            if (!count)
+                count = 3;
 
-                if (!angular.isArray(items) && !angular.isString(items)) return items;
+            if (!angular.isArray(items) && !angular.isString(items)) return items;
 
-                var array = [];
-                for (var i = 0; i < items.length; i++) {
-                    var chunkIndex = parseInt(i / count, 10);
-                    var isFirst = (i % count === 0);
-                    if (isFirst)
-                        array[chunkIndex] = [];
-                    array[chunkIndex].push(items[i]);
-                }
+            var array = [];
+            for (var i = 0; i < items.length; i++) {
+                var chunkIndex = parseInt(i / count, 10);
+                var isFirst = (i % count === 0);
+                if (isFirst)
+                    array[chunkIndex] = [];
+                array[chunkIndex].push(items[i]);
+            }
 
-                if (angular.equals($rootScope.arrayinSliceOf, array))
-                    return $rootScope.arrayinSliceOf;
-                else
-                    $rootScope.arrayinSliceOf = array;
+            if (angular.equals($rootScope.arrayinSliceOf, array))
+                return $rootScope.arrayinSliceOf;
+            else
+                $rootScope.arrayinSliceOf = array;
 
-                return array;
-            };
+            return array;
+        };
 
-            return makeSlices;
-         }]);
+        return makeSlices;
+    }
+]);
