ecd4834d4802d5cbf4b87f38c8aeebace9bb0703
jagruti181 <patiljagruti181@gmail.com>
2015-07-06 11:56:04 +0530

    config file

---
+++ www/js/config.js
@@ -0,0 +1,14 @@
+//FOR TUMBLR INTEGRATION 
+var Tumblr_UserName = "humoristics.tumblr.com";
+
+//FOR FLICKR INTEGRATION
+var Api_Key = "477a6eb655b448de7fffcb16ae4455b2";
+var User_Key = "133690617@N02";
+
+
+angular.module('starter.config', [])
+    .constant('TUBMLR_API_URL', 'http://wohlig.co.in/tumblr/?url=http://api.tumblr.com/v2/blog/' + Tumblr_UserName + '/')
+    .constant('FLICKR_API_URL', 'https://api.flickr.com/services/rest/?&method=flickr.galleries.getList&api_key='+Api_Key+'&user_id='+User_Key+'&format=json&nojsoncallback=1')
+    .constant('FLICKR_GALLERY_API_URL', 'https://api.flickr.com/services/rest/?&method=flickr.galleries.getPhotos&api_key='+Api_Key+'&')
+
+;
--- www/js/service.js
+++ www/js/service.js
@@ -1,34 +1,27 @@
 
-var adminurl = "http://www.wohlig.co.in/LightSaberBackend/index.php/json/";
-
-
 var post = [];
 
 var myservices = angular.module('myservices', []);
 
-myservices.factory('MyServices', function($http) {
+myservices.factory('MyServices', function($http,TUBMLR_API_URL,FLICKR_API_URL) {
 
     var returnval = {};
 
     returnval.getPosts = function(callback) {
-//        return $http.get("http://wohlig.co.in/tumblr/?url=http://api.tumblr.com/v2/blog/holyartsandcraftsbatman.tumblr.com/posts").success(callback);
-        return $http.get("http://wohlig.co.in/tumblr/?url=http://api.tumblr.com/v2/blog/humoristics.tumblr.com/posts").success(callback);
-//        return $http.get("http://wohlig.co.in/tumblr/?url=http://api.tumblr.com/v2/blog/wohligtecjhkjhhnology.tumblr.com/posts").success(callback);
-//        return $http.get("http://wohlig.co.in/tumblr/?url=http://api.tumblr.com/v2/blog/jamiewignall.tumblr.com/posts").success(callback);
+        return $http.get(TUBMLR_API_URL+"posts").success(callback);
     };
     
     returnval.getFlickrGallary = function(callback) {
-        return $http.get("https://api.flickr.com/services/rest/?&method=flickr.galleries.getList&api_key=477a6eb655b448de7fffcb16ae4455b2&user_id=133690617@N02&format=json&nojsoncallback=1").success(callback);
+        return $http.get(FLICKR_API_URL).success(callback);
     };
     
     returnval.getFlickrGallaryPhotos = function(id, callback) {
-        return $http.get("https://api.flickr.com/services/rest/?&method=flickr.galleries.getPhotos&api_key=477a6eb655b448de7fffcb16ae4455b2&gallery_id="+id+"&format=json&nojsoncallback=1").success(callback);
+        return $http.get(FLICKR_GALLERY_API_URL+"gallery_id="+id+"&format=json&nojsoncallback=1").success(callback);
     };
     
     
     returnval.postDetail = function(post) {
         $.jStorage.set("detail",post);
-//        if($.jStorage.get("detail")){callback;}
     };
     
     returnval.getPostDetail = function() {
