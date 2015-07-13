ff6358ed7c83fd7a3ef67dc0eadfa5091be01c17
sohan1 <shn619@gmail.com>
2015-07-13 16:21:29 +0530

    done wordpress

--- www/js/app.js
+++ www/js/app.js
@@ -149,6 +149,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'myservices', 'jagrut
     };
 })
 
+.filter('cut', function () {
+    return function (value, wordwise, max, tail) {
+        if (!value) return '';
+
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace != -1) {
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' …');
+    };
+})
 .filter('rawHtml', ['$sce', function ($sce) {
     return function (val) {
         return $sce.trustAsHtml(val);
@@ -167,4 +186,4 @@ function splitarray(fullarray, splitsize) {
         newarray[arrindex].push(fullarray[i]);
     }
     return newarray;
-};
+}
